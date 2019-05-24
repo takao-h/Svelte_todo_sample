4ec8f51ca169a22f6da90dd03ee8ff5bc2a95635
林 恭央 <k00121@bz4n-034.local.cyberbuzz.co.jp>
2019-05-24 14:24:40 +0900

    todo 完成

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,52 @@
 <script>
-	export let name;
+	let todos = [
+		{ done: false, text: 'add your tasks!' },
+
+	];
+
+	function add() {
+		todos = todos.concat({ done: false, text: '' });
+	}
+
+	function clear() {
+		todos = todos.filter(t => !t.done);
+	}
+
+	$: remaining = todos.filter(t => !t.done).length;
 </script>
 
 <style>
-	h1 {
-		color: purple;
+	.done {
+		opacity: 0.4;
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Todos</h1>
+<button on:click={add}>
+	Add new
+</button>
+
+<button on:click={clear}>
+	Clear completed
+</button>
+{#each todos as todo}
+	<div class:done={todo.done}>
+		<input
+			type=checkbox
+			bind:checked={todo.done}
+		>
+
+		<input
+			placeholder="What needs to be done?"
+			bind:value={todo.text}
+		>
+	</div>
+{/each}
+
+{#if remaining > 5}
+  <p>💀💀that's too match tasks💀💀</p>
+{:else}
+  <p>{remaining} remaining</p>
+{/if}
+
+
